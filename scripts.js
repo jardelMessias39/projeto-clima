55446fc99eca8aa4ddee05876cfb819e4f84fdf4
jardelMessias39 <jardelmessias28@gmail.com>
2026-02-17 00:06:19 -0300

    vai

--- scripts.js
+++ scripts.js
@@ -324,9 +324,9 @@ function detectarVoz() {
 }
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then(() => console.log("PWA ativo"))
+    navigator.serviceWorker.register("service-worker.js")
+      .then(() => console.log("SW registrado"))
       .catch(err => console.log("Erro SW:", err));
   });
 }
+
