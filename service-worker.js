a9290ef390fae83ef31f234ff850530259f15824
jardelMessias39 <jardelmessias28@gmail.com>
2026-02-17 00:03:23 -0300

    add

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,23 @@
-const CACHE_NAME = "clima-v1";
+const CACHE_NAME = "clima-v2";
 
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/styles.css",
-  "/scripts.js"
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./scripts.js",
+  "./manifest.json",
+  "./img/icon-192.png",
+  "./img/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log("Cache aberto");
+        return cache.addAll(urlsToCache);
+      })
+      .catch(err => console.error("Erro ao cachear:", err))
   );
 });
 
